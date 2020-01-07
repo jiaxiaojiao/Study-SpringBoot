2991f4d0a031e179e507cd4e3d3a19fb592d93aa
v-jiaxiaojiao <v-jiaxiaojiao2019@jd.com>
2020-01-07 14:34:23 +0800

    user-provider 把Dubbo配置 从公共配置application.properties 拆出来。

--- usercenter/user-web/src/main/java/com/jxj/user/UserWebApplication.java
+++ usercenter/user-web/src/main/java/com/jxj/user/UserWebApplication.java
@@ -1,13 +1,9 @@
 package com.jxj.user;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@NacosPropertySource(dataId = "user.example", autoRefreshed = true)
-@EnableDubbo(scanBasePackages = "com.jxj.user.service")
 public class UserWebApplication {
 
     public static void main(String[] args) {
