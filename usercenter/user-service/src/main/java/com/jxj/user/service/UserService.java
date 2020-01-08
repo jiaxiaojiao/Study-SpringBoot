2e4c6a9cc662d177b4f5a014a2732640886f89bf
v-jiaxiaojiao <v-jiaxiaojiao2019@jd.com>
2020-01-08 16:34:39 +0800

    user-provider 添加注释

--- usercenter/user-service/src/main/java/com/jxj/user/service/UserService.java
+++ usercenter/user-service/src/main/java/com/jxj/user/service/UserService.java
@@ -1,20 +1,35 @@
 package com.jxj.user.service;
 
-
 import com.jxj.user.entity.vo.UserVO;
 
 import java.util.List;
 
 public interface UserService {
+    /**
+     * 用户注册
+     * @param user
+     * @return
+     */
     int signIn(UserVO user);
 
+    /**
+     * 用户列表（所有用户）
+     * @return
+     */
     List<UserVO> list();
 
+    /**
+     * 查询用户（通过主键用户ID）
+     * @param id
+     * @return
+     */
     UserVO findUser(Long id);
 
     /**
-     * 参数name是否可用：
+     * 用户名校验
      * true 可用，false 不可用
+     * @param name
+     * @return
      */
     boolean usableName(String name);
 }
