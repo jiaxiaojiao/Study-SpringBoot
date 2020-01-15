643e336d6aa1b47046bd715599db94f75bffc0ce
v-jiaxiaojiao <v-jiaxiaojiao2019@jd.com>
2020-01-15 11:15:10 +0800

    user-provider 1. 暴露监控端点，借助 Micrometer 对接 Prometheus 监控系统 2. Spring Boot版本修改为2.1.3.RELEASE。 原因： 版本2.2.2.RELEASE某些方法缺失

--- usercenter/user-web/src/main/java/com/jxj/user/UserWebApplication.java
+++ usercenter/user-web/src/main/java/com/jxj/user/UserWebApplication.java
@@ -1,11 +1,13 @@
 package com.jxj.user;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableDubbo(scanBasePackages = "com.jxj.user.service.impl")
 public class UserWebApplication {
 
     public static void main(String[] args) {
--- usercenter/user-web/src/main/java/com/jxj/user/configuration/ProviderConfiguration.java
+++ usercenter/user-web/src/main/java/com/jxj/user/configuration/ProviderConfiguration.java
@@ -1,6 +1,5 @@
 package com.jxj.user.configuration;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.PropertySource;
  * 指定Spring扫描路径
  */
 @Configuration
-@EnableDubbo(scanBasePackages = "com.jxj.user.service.impl")
 @PropertySource("classpath:/dubbo-provider.properties")
 public class ProviderConfiguration {
 }
